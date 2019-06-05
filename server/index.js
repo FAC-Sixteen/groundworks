97fdc96fe27fad4f3148bc20fb04be6177b0bb1c
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-05 12:08:21 +0100

    feature/ add comment lines to code

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
-const app = require('./app');
-const mongoose = require('mongoose');
-const env = require('env2')('./config.env');
+const app = require("./app"); // requiring app.js from ./app file
+const mongoose = require("mongoose"); // Framework for MongoDB
+const env = require("env2")("./config.env"); // used to hide Keys
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; //Setting port on which we can view backend.
 
-app.listen(port, () =>
-  { console.log(`Node/Express server running on port ${port}`);})
+// Listening to see if server is up and running.
+app.listen(port, () => {
+  console.log(`Node/Express server running on port ${port}`);
+});
 
+// mongoose got updated and now this is essiential.
 const options = {
   useNewUrlParser: true
-}
+};
 
-mongoose.connect(process.env.DB_CONNECTION, options)
+mongoose.connect(process.env.DB_CONNECTION, options); // initial connection.
 
 const db = mongoose.connection;
 
-db.once('open', () => console.log('connected to the database'))
-db.on('error', console.error.bind(console, 'MongoDB connection error'));
+db.once("open", () => console.log("connected to the database")); // Opening Connection.
+db.on("error", console.error.bind(console, "MongoDB connection error")); // catching errors.
