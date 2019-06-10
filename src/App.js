4fbf23372546c015cbb44050eebf318c6472b845
dalmano <dalmano@outlook.com>
2019-06-10 18:11:10 +0100

    create header component Relates #36

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Info from "./components/Homepage/Info/Info";
+import Header from "./components/Homepage/Header/Header";
 
 const App = () => {
   return (
     <div>
       <h1>Client Side ReactApp</h1>
+      <Header />
       <Info />
     </div>
   );
