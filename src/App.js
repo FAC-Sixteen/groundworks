ef85076469d1ba832d4dc6b5972aca825e51a290
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-10 18:07:42 +0100

    feature/add footer #33

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import NavBar from "./components/Navbar/NavBar";
+import Footer from "./components/Footer/Footer";
 import "./normalize.css";
 import "./app.css";
 
 import Info from "./components/Homepage/Info/Info";
-
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Info />
+      <Footer />
     </div>
   );
 };
