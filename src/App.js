dd2af762e1cd4760c8322b37ca8d7f2a4ab95db9
James Levine <jamesalexanderlevine@gmail.com>
2019-06-10 18:10:56 +0100

    added founders component relating to issue #33

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Info from "./components/Homepage/Info/Info";
+import Founders from "./components/Homepage/Founders/Founders";
 
 const App = () => {
   return (
     <div>
       <h1>Client Side ReactApp</h1>
       <Info />
+      <Founders />
     </div>
   );
 };
