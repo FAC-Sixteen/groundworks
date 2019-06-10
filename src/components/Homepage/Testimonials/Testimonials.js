ed3b364f3bd228e6b8cb8dced8b21c78bcd42cbc
misterrodger <johndavidrodger@gmail.com>
2019-06-10 19:09:30 +0300

    Static Testimonials component created
    - Can update to use multiple testimonials either from database or static data
    - Relates #29

---
+++ src/components/Homepage/Testimonials/Testimonials.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './Testimonials.css'
+
+const Testimonials = () => {
+
+  return(
+    <div>
+        <p>"Thanks to the project I was assigned at Groundworks I now have more real-life work experience and a sense of career direction."</p>
+        <span>Steven, University of Briston, 2019</span>
+    </div>
+
+  )
+}
+
+export default Testimonials;
