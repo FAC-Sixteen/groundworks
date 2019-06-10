10b299c51ea7e0e67fd0f1b80eb5998cd5b73d26
dalmano <dalmano@outlook.com>
2019-06-10 16:53:46 +0100

    create info component Relates #30

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Info from "./components/Homepage/Info/Info";
 
 const App = () => {
-    return (
-      <div>
-        <h1>Client Side ReactApp</h1>
-      </div>
-    );
-}
+  return (
+    <div>
+      <h1>Client Side ReactApp</h1>
+      <Info />
+    </div>
+  );
+};
 
 export default App;
