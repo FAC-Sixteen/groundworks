903fc23b31c1da00da210c40bc6c7bf7d004011a
James Levine <jamesalexanderlevine@gmail.com>
2019-06-11 15:01:49 +0100

    toggle button component relates #49

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import NavBar from "./components/Navbar/NavBar";
-import Footer from "./components/Footer/Footer";
 import "./normalize.css";
 import "./app.css";
 
 import Info from "./components/Homepage/Info/Info";
 import Header from "./components/Homepage/Header/Header";
 import Founders from "./components/Homepage/Founders/Founders";
+import NavBar from "./components/Navbar/NavBar";
+import Footer from "./components/Footer/Footer";
+import ClientJobsToggle from "./components/ClientDashboard/ClientJobsToggle/ClientJobsToggle";
 import Ourplatform from "./components/Homepage/Ourplatform/Ourplatform";
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <Info />
       <Ourplatform />
       <Founders />
+      <ClientJobsToggle />
       <Footer />
     </div>
   );
---
+++ src/components/ClientDashboard/ClientJobsToggle/ClientJobsToggle.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ClientJobsToggle.css";
+
+const ClientJobsToggle = () => {
+  return (
+    <div class="ClientJobsToggle--container">
+    <div class="ClientJobsToggle--div">New</div>
+    <div class="ClientJobsToggle--div">Live</div>
+    <div class="ClientJobsToggle--div">Completed</div>
+    </div>
+  );
+};
+
+export default ClientJobsToggle;
