6dffd9eb12e46b9ee6757251400316734b0a1dab
misterrodger <johndavidrodger@gmail.com>
2019-06-12 01:16:08 +0300

    Merge App.js file
    - Conflicts between branches
    Relates #44

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
-import NavBar from "./components/Navbar/NavBar";
-import Footer from "./components/Footer/Footer";
 import "./normalize.css";
 import "./App.css";
 
-import Info from "./components/Homepage/Info/Info";
+import NavBar from "./components/Navbar/NavBar";
 import Header from "./components/Homepage/Header/Header";
+import Info from "./components/Homepage/Info/Info";
+import Ourplatform from "./components/Homepage/Ourplatform/Ourplatform.js";
 import Testimonials from "./components/Homepage/Testimonials/Testimonials.js";
 import Founders from "./components/Homepage/Founders/Founders";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
-      <h1>Client Side ReactApp</h1>
-      <Header />
       <NavBar />
+      <Header />
       <Info />
+      <Ourplatform />
       <Testimonials />
       <Founders />
       <Footer />
