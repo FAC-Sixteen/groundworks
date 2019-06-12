a0cb63b1b4189d2da452bbe26278a8706cb95171
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-12 10:38:17 +0100

    add basic login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import NavBar from "./components/Navbar/NavBar";
-import Footer from "./components/Footer/Footer";
 import "./normalize.css";
 import "./app.css";
 
+import NavBar from "./components/Navbar/NavBar";
+import Footer from "./components/Footer/Footer";
+import LoginPortal from "./components/LoginRegister/LoginPortal/LoginPortal";
 import Info from "./components/Homepage/Info/Info";
 import Header from "./components/Homepage/Header/Header";
 import Founders from "./components/Homepage/Founders/Founders";
@@ -11,9 +12,8 @@ import Founders from "./components/Homepage/Founders/Founders";
 const App = () => {
   return (
     <div>
-      <h1>Client Side ReactApp</h1>
-      <Header />
       <NavBar />
+      <Header />
       <Info />
       <Founders />
       <Footer />
