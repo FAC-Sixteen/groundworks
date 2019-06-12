6486ecd5b0900a0448fc6d4dce8a229d99aec3f4
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-12 10:49:11 +0100

    Fix/ master branch

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import "./normalize.css";
 import "./App.css";
 
-import NavBar from "./components/Navbar/NavBar";
-import Footer from "./components/Footer/Footer";
 import LoginPortal from "./components/LoginRegister/LoginPortal/LoginPortal";
 import Info from "./components/Homepage/Info/Info";
 import Header from "./components/Homepage/Header/Header";
