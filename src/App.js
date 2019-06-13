a90b85a22530a0df343486a97e47d124c718e1cd
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-13 12:50:44 +0100

    feature/set up route

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from "react";
 import "./normalize.css";
-import Home from './routes/Home';
-import Login from './routes/Login';
-import StudentSignup from './routes/StudentSignup';
-import ClientSignup from './routes/ClientSignup';
-import StudentDashboard from './routes/StudentDashboard';
-import ClientDashboard from './routes/ClientDashboard';
-import NewBrief from './routes/NewBrief';
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import Join from "./routes/Join";
+import StudentSignup from "./routes/StudentSignup";
+import ClientSignup from "./routes/ClientSignup";
+import StudentDashboard from "./routes/StudentDashboard";
+import ClientDashboard from "./routes/ClientDashboard";
+import NewBrief from "./routes/NewBrief";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/" exact component={ Home } />
-          <Route path="/Login" component={ Login } />
-          <Route path="/StudentSignup" component={ StudentSignup } />
-          <Route path="/ClientSignup" component={ ClientSignup } />
-          <Route path="/StudentDashboard" component={ StudentDashboard } />
-          <Route path="/ClientDashboard" component={ ClientDashboard } />
-          <Route path="/NewBrief" component={ NewBrief } />
+          <Route path="/" exact component={Home} />
+          <Route path="/Login" component={Login} />
+          <Route path="/Join" component={Join} />
+          <Route path="/StudentSignup" component={StudentSignup} />
+          <Route path="/ClientSignup" component={ClientSignup} />
+          <Route path="/StudentDashboard" component={StudentDashboard} />
+          <Route path="/ClientDashboard" component={ClientDashboard} />
+          <Route path="/NewBrief" component={NewBrief} />
         </Switch>
       </div>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
