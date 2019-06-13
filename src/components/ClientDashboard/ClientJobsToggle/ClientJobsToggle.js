6d92fdc38b19fe73d13a0bbf3ae34cc7db17b3ee
James Levine <jamesalexanderlevine@gmail.com>
2019-06-13 13:32:51 +0100

    student toggle button and client toggle button completed

--- src/components/ClientDashboard/ClientJobsToggle/ClientJobsToggle.js
+++ src/components/ClientDashboard/ClientJobsToggle/ClientJobsToggle.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ClientJobsToggle.css";
 
 const ClientJobsToggle = () => {
+  const [grey1, setGrey1] = useState(false);
+  const [grey2, setGrey2] = useState(false);
+  const [grey3, setGrey3] = useState(false);
+
+  const change1 = () => {
+    setGrey1(!grey1);
+    setGrey2(false);
+    setGrey3(false);
+  };
+
+  const change2 = () => {
+    setGrey2(!grey2);
+    setGrey1(false);
+    setGrey3(false);
+  };
+
+  const change3 = () => {
+    setGrey3(!grey3);
+    setGrey1(false);
+    setGrey2(false);
+  };
+
   return (
     <div className="ClientJobsToggle--container">
-      <div className="ClientJobsToggle--div">New</div>
-      <div className="ClientJobsToggle--div">Live</div>
-      <div className="ClientJobsToggle--div">Completed</div>
+      <div className={grey1 ? "grey1" : "ClientJobsToggle--div1"} onClick={change1}>New</div>
+      <div className={grey2 ? "grey2" : "ClientJobsToggle--div2"} onClick={change2}>Live</div>
+      <div className={grey3 ? "grey3" : "ClientJobsToggle--div3"} onClick={change3}>Completed</div>
     </div>
   );
 };
--- src/components/StudentDashboard/StudentJobsToggle/StudentJobsToggle.js
+++ src/components/StudentDashboard/StudentJobsToggle/StudentJobsToggle.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./StudentJobsToggle.css";
 
 const StudentJobsToggle = () => {
+  const [grey1, setGrey1] = useState(false);
+  const [grey2, setGrey2] = useState(false);
+  const [grey3, setGrey3] = useState(false);
+
+  const change1 = () => {
+    setGrey1(!grey1);
+    setGrey2(false);
+    setGrey3(false);
+  };
+
+  const change2 = () => {
+    setGrey2(!grey2);
+    setGrey1(false);
+    setGrey3(false);
+  };
+
+  const change3 = () => {
+    setGrey3(!grey3);
+    setGrey1(false);
+    setGrey2(false);
+  };
+
   return (
     <div className="StudentJobsToggle--container">
-      <div className="StudentJobsToggle--div">New</div>
-      <div className="StudentJobsToggle--div">Live</div>
-      <div className="StudentJobsToggle--div">Completed</div>
+      <div className={grey1 ? "grey1" : "StudentJobsToggle--div1"} onClick={change1}>New</div>
+      <div className={grey2 ? "grey2" : "StudentJobsToggle--div2"} onClick={change2}>Live</div>
+      <div className={grey3 ? "grey3" : "StudentJobsToggle--div3"} onClick={change3}>Completed</div>
     </div>
   );
 };
