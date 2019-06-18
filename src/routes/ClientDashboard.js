ecbae15884f2b54c38bcb0cabb18c38cc36adab5
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-18 21:31:53 +0100

    add register portal

--- src/routes/ClientDashboard.js
+++ src/routes/ClientDashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import ClientJobsToggle from "../components/ClientDashboard/ClientJobsToggle/ClientJobsToggle";
 import ClientProfileCard from "../components/ClientDashboard/ClientProfileCard/ClientProfileCard";
 import ClientCompletedJobCard from "../components/ClientDashboard/CompletedJobCard/ClientCompletedJobCard";
-import ClientCurrentJobCard from "../components/StudentDashboard/CurrentJobCard/CurrentJobCard";
+import ClientCurrentJobCard from "../components/ClientDashboard/CurrentJobCard/CurrentJobCard";
 import LongStudentProfileCard from "../components/ClientDashboard/LongStudentProfileCard/LongStudentProfileCard";
 
 const ClientDashboard = () => {
