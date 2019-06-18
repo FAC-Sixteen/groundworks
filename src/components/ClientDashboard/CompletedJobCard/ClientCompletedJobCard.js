42b2e225b3254aa788dd8b700ad6f13cb0af2e3c
James Levine <jamesalexanderlevine@gmail.com>
2019-06-18 21:37:15 +0100

    finished completed job card

--- src/components/ClientDashboard/CompletedJobCard/ClientCompletedJobCard.js
+++ src/components/ClientDashboard/CompletedJobCard/ClientCompletedJobCard.js
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ClientCompletedJobCard.css";
+import axios from "axios";
 
 const ClientJobCard = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("/api/client/all-briefs");
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="ClientJobCard">
-      <div className="ClientJobCard--container">
-        <h2 className="ClientJobCard--projectname">Project Name</h2>
-        <p className="ClientJobCard--projectdesc">
-          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
-          Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-          Lorem Ipsum
-        </p>
-        <div className="ClientJobCard--contactcontainer">
-          <div className="ClientJobCard--contactdetailsdiv">
-            <h4>Contact Email</h4>
-            <h4>Contact Email</h4>
-            <h4>Contact Email</h4>
-          </div>
-          <div className="ClientJobCard--projectdetailsdiv">
-            <h4>Price</h4>
-            <h4>Workload</h4>
-            <h4>Deadline</h4>
-          </div>
-        </div>
-        <div className="ClientJobCard--downloadbrief">
-          <span className="ClientJobCard--downloadbrief__name">
-            Brief File Name
-          </span>
-          <button>Download</button>
-        </div>
-      </div>
-    </div>
+    <ul>
+      {data.map((value, index) => {
+        return (
+          <li key={index}>
+            {
+              <div className="ClientJobCard">
+                <div className="ClientJobCard--container">
+                  <h2 className="ClientJobCard--projectname">
+                    Project Name: {value.projectName}
+                  </h2>
+                  <p className="ClientJobCard--projectdesc">
+                    Project Brief: {value.projectBrief}
+                  </p>
+                  <div className="ClientJobCard--contactcontainer">
+                    <div className="ClientJobCard--contactdetailsdiv">
+                      <h4>Company Name: {value.companyName}</h4>
+                      <h4>Contact Person: {value.contactPerson}</h4>
+                    </div>
+                    <div className="ClientJobCard--projectdetailsdiv">
+                      <h4>Project Price: {value.projectPrice}</h4>
+                      <h4>Estimated Workload: {value.estimatedWorkload}</h4>
+                      <h4>Project Deadline: {value.projectDeadline}</h4>
+                    </div>
+                  </div>
+                  <div className="ClientJobCard--downloadbrief">
+                    <span className="ClientJobCard--downloadbrief__name">
+                      Brief File Name
+                    </span>
+                    <button>Download</button>
+                  </div>
+                </div>
+              </div>
+            }
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
