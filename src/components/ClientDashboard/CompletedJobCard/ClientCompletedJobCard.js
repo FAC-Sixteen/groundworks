bb51948d7f6f6a88667abd279e69dc8b6970584d
dalmano <dalmano@outlook.com>
2019-06-19 11:38:48 +0100

    Client job card

--- src/components/ClientDashboard/CompletedJobCard/ClientCompletedJobCard.js
+++ src/components/ClientDashboard/CompletedJobCard/ClientCompletedJobCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ClientCompletedJobCard.css";
+import axios from "axios";
 
 const ClientJobCard = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("/api/client/all-briefs");
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="ClientJobCard">
       <div className="ClientJobCard--container">
         <h2 className="ClientJobCard--projectname">Project Name</h2>
-        <p className="ClientJobCard--projectdesc">
-          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
-          Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-          Lorem Ipsum
-        </p>
+        <p className="ClientJobCard--projectdesc">which one is this</p>
         <div className="ClientJobCard--contactcontainer">
           <div className="ClientJobCard--contactdetailsdiv">
             <h4>Contact Email</h4>
